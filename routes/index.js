c53892b5c4826bf886152bc0497b28c3a62be8a7
LizTheDeveloper <liz.kelner@gmail.com>
2015-03-05 11:18:43 -0800

    Got the server sending cookies to the browser, and then using them to conditionally serve a different page for a logged in user

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log("in the home route")
+	console.log(req.cookies["email"])
+	if (req.cookies["email"]) {
+		var user = {
+			name : req.cookies["name"],
+			email : req.cookies["email"]
+		};
+		res.render('home.ejs', {"user": user}, function(err,html){
+	  		res.send(html);
+	  	});
+	}
+
+
   	res.render('index.ejs', {}, function(err,html){
   		res.send(html);
   	});
@@ -10,6 +23,11 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
 	saveUserInDB(req);
+
+	//Log in user
+	res.cookie("name", req.body.name);
+	res.cookie("email", req.body.email);
+
 	var user = {
 		name : req.body.name,
 		email : req.body.email,
